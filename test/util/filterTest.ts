83af7cdb1477a79d75c664b231f1c6ffd13bc179
Matthieu Bosquet <matthieubosquet@gmail.com>
2021-03-08 17:09:52 +0000

    Refactor tests

--- test/util/filterTest.ts
+++ test/util/filterTest.ts
@@ -1,12 +1,26 @@
 import type { ITestCase, ITestRequirement } from "../type";
 
-export const filterRequirements = (requirements: ITestRequirement[], filter: string): ITestCase[] => {
+export const filterRequirements = (
+  requirements: ITestRequirement[],
+  filter: string
+): ITestCase[] => {
   return requirements
-    .map(x => x.cases)
-    .reduce((accumulator: ITestCase[], value) => { return [ ...accumulator, ...value ]})
-    .filter(test => Array.isArray(test.seeAlso) && test.seeAlso.some(seeAlso => seeAlso === filter));
-}
+    .map((x) => x.cases)
+    .reduce((accumulator: ITestCase[], value) => [...accumulator, ...value])
+    .filter(
+      (testCase) =>
+        Array.isArray(testCase.seeAlso) &&
+        testCase.seeAlso.some((seeAlso) => seeAlso === filter)
+    );
+};
 
-export const filterCases = (testCases: ITestCase[], filter: string): ITestCase[] => {
-  return testCases.filter(test => Array.isArray(test.seeAlso) && test.seeAlso.some(seeAlso => seeAlso === filter));
-}
+export const filterCases = (
+  testCases: ITestCase[],
+  filter: string
+): ITestCase[] => {
+  return testCases.filter(
+    (testCase) =>
+      Array.isArray(testCase.seeAlso) &&
+      testCase.seeAlso.some((seeAlso) => seeAlso === filter)
+  );
+};
--- test/util/runTest.ts
+++ test/util/runTest.ts
@@ -1,11 +1,17 @@
 import type { ITestCase, ITestRequirement } from "../type";
 
 export const runTest = (requirement: ITestRequirement): void => {
-  describe(requirement.label, () => {
-    requirement.cases.forEach((testCase: ITestCase) => {
-      it(testCase.description, () => {
-        expect(testCase.input).toEqual(testCase.output);
+  describe(
+    requirement.label.trim().concat(" ", requirement.description.trim()).trim(),
+    () => {
+      requirement.cases.forEach((testCase: ITestCase) => {
+        it(
+          testCase.label.trim().concat(" ", testCase.description.trim()).trim(),
+          () => {
+            expect(testCase.input).toEqual(testCase.output);
+          }
+        );
       });
-    });
-  });
+    }
+  );
 };
